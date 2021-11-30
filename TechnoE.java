9526e1a65b3547287fa70043256f3530de8c387e
kratikakeer <keerkratika212@gmail.com>
2021-11-30 09:08:59 +0530

    java files

---
+++ TechnoE.java
@@ -0,0 +1,12 @@
+class TechnoE
+{
+public static void main(String []args)
+{
+System.out.println("WELCOME TO TECHNOELEVATE");
+
+
+int x=123,y=456;
+int sum=Integer.sum(x,y);
+System.out.println("Addition is "+sum);
+}
+}
